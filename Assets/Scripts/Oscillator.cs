3639d253c51bad4edf27a196349829be7450ae91
bongster228 <43600850+bongster228@users.noreply.github.com>
2019-11-28 23:49:21 -0800

    Created 4 levels

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+
+    [SerializeField] float period = 2f;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField][Range(0,1)] float movementFactor;
+    Vector3 startingPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  error check for when period == 0
+        if (period <= Mathf.Epsilon) return;
+        
+        //  cycle gradually increases from 0
+        float cycle = Time.time / period;
+
+        //  oscillate between 0 and 1
+        const float tau = Mathf.PI * 2;
+        float rawSineWave = Mathf.Sin(tau * cycle);
+        movementFactor = (rawSineWave / 2) + .5f;
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos+ offset;
+    }
+}
--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,36 +1,140 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
 
-    [SerializeField] float lrThrust = 10f;
-    [SerializeField] float thrust = 20f;
+    [SerializeField] float rotationMultiplier = 10f;
+    [SerializeField] float thrustMultiplier = 20f;
 
+    [SerializeField] float levelLoadDelay = 2f;
+
+    [SerializeField] AudioClip thrustSound;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] AudioClip successSound;
+
+    [SerializeField] ParticleSystem mainEngineParticle;
+    [SerializeField] ParticleSystem successParticle;
+    [SerializeField] ParticleSystem deathParticle;
+
+    AudioSource audioSource;
     Rigidbody rigidbody;
+
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+        //  todo stop sound on death
+        if (state == State.Alive)
         {
-            rigidbody.AddRelativeForce(Vector3.up * thrust * Time.deltaTime);
+            RespondToThrustInput();
+            Rotate();
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (state != State.Alive) return;
+
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+    }
+
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+
+        //  play success animation and sound
+        successParticle.Play();
+        audioSource.PlayOneShot(successSound);
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+
+        //  play death animation and sound
+        audioSource.PlayOneShot(deathSound);
+        deathParticle.Play();
+        Invoke("LoadFirstLevel", levelLoadDelay);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void Rotate()
+    {
+        //  Freeze rotation when there is no user input
+        rigidbody.freezeRotation = true;
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationMultiplier * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationMultiplier * Time.deltaTime);
         }
+
+        rigidbody.freezeRotation = false;
+    }
+
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ApplyThrust();
+        }
+        else
+        {
+            //  Stop engine sound and particle when there is no input
+            audioSource.Stop();
+            mainEngineParticle.Stop();
+        }
+    }
+
+    private void ApplyThrust()
+    {
+        rigidbody.AddRelativeForce(Vector3.up * thrustMultiplier * Time.deltaTime);
+
+        //  prevent engine sound layering
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(thrustSound);
+        }
+        mainEngineParticle.Play();
     }
 }
